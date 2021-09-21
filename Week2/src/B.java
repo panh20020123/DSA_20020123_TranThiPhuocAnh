f7bf672365d2f008797a2b85abdaa9fa747dd789
panh20020123 <tranpanh55@gmail.com>
2021-09-21 23:41:31 +0700

    Tuan1 va Tuan 2

---
+++ Week2/src/B.java
@@ -0,0 +1,6 @@
+public class B {
+    public static void main(String[] args) {
+        int x = 2, y = 3;
+        System.out.println(A.tong(x, y) + " " + A.binh(x) + " " + A.binh(y));
+    }
+}
---
+++ Week2/src/lib/A.java
@@ -0,0 +1,8 @@
+class A {
+    public static int tong(int x, int y) {
+        return x + y;
+    }
+    public static int binh(int x) {
+        return x * x;
+    }
+}
