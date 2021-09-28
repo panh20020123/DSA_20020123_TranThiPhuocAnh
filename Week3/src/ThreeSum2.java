a5f576de62b7a5b2016172f9c7877a5b528f76fe
panh20020123 <tranpanh55@gmail.com>
2021-09-28 23:48:15 +0700

    fix bai ThreeSum2 week3

--- Week3/src/ThreeSum2.java
+++ Week3/src/ThreeSum2.java
@@ -3,27 +3,32 @@ import edu.princeton.cs.algs4.StdArrayIO;
 import java.util.Arrays;
 
 public class ThreeSum2 {
+    public static void printAll(int[] a) {
+        int n = a.length;
+        Arrays.sort(a);
+        int l, r;
+
+        for (int i = 0; i < n - 2; i++) {
+            l = i + 1;
+            r = n - 1;
+            while (l < r) {
+                if (a[i] + a[l] + a[r] == 0) {
+                    System.out.println(a[i] + ", " + a[l] + ", " + a[r]);
+                    l++;
+                    r--;
+                } else if (a[i] + a[l] + a[r] < 0) {
+                    l++;
+                } else { // A[i] + A[l] + A[r] > 0
+                    r--;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         In in = new In ("D:\\algs4-data\\8Kints.txt"); //tao luong doc
         int[] a = in.readAllInts(); // doc vao mang a
-        int n= a.length;
-        Arrays.sort(a);
-        for(int i=0; i<n-2; i++)
-            for(int j=i+1; j<n-1; j++)
-            {
-            int dau = j+1;
-            int cuoi = n-1;
-            while (dau <= cuoi)
-            {
-                int giua = (dau + cuoi)/2;
-                if (a[i]+a[giua]+a[j]==0) {System.out.println(a[i]+" "+a[j]+" "+a[giua]); break;}
-                else if(a[i]+a[j]+a[giua] < 0) dau = giua+1;
-                else if(a[i]+a[j]+a[giua] > 0) cuoi = giua-1;
-
-            }
-            }
-
-        //StdArrayIO.print(a); // in mang a ra mh
+        printAll(a);
     }
 }
 
