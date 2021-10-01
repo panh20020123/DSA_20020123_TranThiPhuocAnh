4ca93459b6d8367fb2f67aaaee75e2ec1162951c
panh20020123 <tranpanh55@gmail.com>
2021-10-01 18:52:09 +0700

    fix Pairs gon gang hon

--- Week3/src/Pairs.java
+++ Week3/src/Pairs.java
@@ -1,22 +1,20 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Pairs {
     public static int pairs(int k, List<Integer> arr) {
         int n = arr.size();
         int kq = 0;
-        int[] a = new int[n];
+        //Collections.sort(arr);
+        arr.sort((o1,o2) -> o2-o1 );
 
-        for (int i=0; i < n; i++) {
-            a[i] = arr.get(i);
-        }
-        Arrays.sort(a);
         for (int i = 0; i < n; i++)
         {
             //int m = a[i]+k;
-            int s = Arrays.binarySearch(a,a[i] + k);
-            if(s >= 0) kq++;
+            if(arr.contains(arr.get(i) + k))
+                kq++;
         }
         return kq;
     }
